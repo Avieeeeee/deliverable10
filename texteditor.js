cc3c8c2da082e7168b7d0f62000b5e232ac8c237
Avieeeeee <bosten.torres.yb2021@gmail.com>
2022-07-19 12:55:18 -0700

    seperated js code from html

--- texteditor.js
+++ texteditor.js
@@ -1,30 +1,29 @@
-<script>           
-            function bold() {
-                document.getElementById("textarea1").style.fontWeight = "bold";
-            }
-            function italic() {
-                document.getElementById("textarea1").style.fontStyle = "italic";
-            }
-            function left() {
-                document.getElementById("textarea1").style.textAlign = "left";
-            }
-            function center() {
-                document.getElementById("textarea1").style.textAlign= "center";
-            }
-            function right() {
-                document.getElementById("textarea1").style.textAlign = "right";
-            }
-            function uppercase() {
-                document.getElementById("textarea1").style.textTransform = "uppercase";
-            }
-            function lowercase() {
-                document.getElementById("textarea1").style.textTransform = "lowercase";
-            }
-            function capitalize() {
-                document.getElementById("textarea1").style.textTransform = "capitalize";
-            }
-            function cleartext() {
-                document.getElementById("textarea1").removeAttribute('style')
-                document.getElementById("textarea1").value = ""
-            }
-        </script>
+          
+    function bold() {
+        document.getElementById("textarea1").style.fontWeight = "bold";
+    }
+   function italic() { 
+        document.getElementById("textarea1").style.fontStyle = "italic";
+    }
+   function left() {
+        document.getElementById("textarea1").style.textAlign = "left";
+    }
+    function center() {
+        document.getElementById("textarea1").style.textAlign= "center";
+    }
+    function right() {
+        document.getElementById("textarea1").style.textAlign = "right";
+    }
+    function uppercase() {
+        document.getElementById("textarea1").style.textTransform = "uppercase";
+    }
+    function lowercase() {
+        document.getElementById("textarea1").style.textTransform = "lowercase";
+    }
+    function capitalize() {
+        document.getElementById("textarea1").style.textTransform = "capitalize";
+    }
+    function cleartext() {
+        document.getElementById("textarea1").removeAttribute('style')
+        document.getElementById("textarea1").value = ""
+    }
